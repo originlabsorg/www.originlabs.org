c1434ab8a1862e22c1463c132b8f861afcc1925f
Alex Skryl <rut216@gmail.com>
2018-11-21 20:39:33 -0800

    turn off firebase

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import firebase from '../services/Firebase'
+// import firebase from '../services/Firebase'
 import CustomRecaptcha from './CustomRecaptcha'
 import { reCaptcha, staticmanEndpoint } from '../config'
 import $ from "jquery";
@@ -56,17 +56,17 @@ class Form extends Component {
         this.setState({ sendingButtonMessage: 'Sending...' })
 
         // save form to firebase
-        firebase
-            .database()
-            .ref('/contacts')
-            .push(data)
-            .then(() => {
-                this.setState({ submissionResponse: 'Processing...' })
+        // firebase
+        //     .database()
+        //     .ref('/contacts')
+        //     .push(data)
+        //     .then(() => {
+        //         this.setState({ submissionResponse: 'Processing...' })
 
-            })
-            .catch((error) => {
-                this.setState({ submissionResponse: 'Something went wrong!' })
-            })
+        //     })
+        //     .catch((error) => {
+        //         this.setState({ submissionResponse: 'Something went wrong!' })
+        //     })
 
         // submit form to staticman server
         const data = $(ev.target).serialize()
