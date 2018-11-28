7eeb1d93ccd6219cd4d3dd7c507e93640e662f99
Alex Skryl <rut216@gmail.com>
2018-11-27 16:52:35 -0800

    update staticman endpoint/config

--- src/config/staticman.js
+++ src/config/staticman.js
@@ -1,3 +1,3 @@
 export default {
-  endpoint: 'https://staticman3.herokuapp.com/v2/entry/originlabsorg/originlabs.org/master/comments'
+  endpoint: 'https://staticman3.herokuapp.com/v2/entry/originlabsorg/www.originlabs.org/master/comments'
 }
